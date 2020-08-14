545710ac0d84cc6110638da5d3bd15d77c7650bc
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-15 00:34:59 +0430

    implements users/verify/send route and code

---
+++ src/routes/api/v1/users/verify-send.ts
@@ -0,0 +1,36 @@
+import {BaseController, getRandomToken, handleUnique, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {ValidationChain} from "express-validator";
+import {IUserDoc} from "@models/User";
+import Verification, {verificationTypes} from "@models/Verification";
+
+type localRequestHandler = RequestHandler<{}, { msg: string }, {}, {}>
+
+class VerifySend extends BaseController<localRequestHandler> {
+    exactAccess = true;
+    readonly access = Roles.unverified;
+    readonly method = 'get';
+    readonly path: string = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const user = req.user as IUserDoc;
+            const verificationDoc = new Verification({
+                token: await getRandomToken(),
+                data: {for: verificationTypes.emailVerification, email: user.email}
+            })
+            await verificationDoc.save().catch(handleUnique('duplicate verification data'));
+            console.log(verificationDoc.token) //todo add email sender method
+            res.json({msg: 'success'});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new VerifySend();
