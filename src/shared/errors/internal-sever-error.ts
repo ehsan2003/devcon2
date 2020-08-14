b8b0e22417d7d9cf811c2f6f901b30ff5a869977
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-14 23:12:36 +0430

    changes internal server error message
    changes internal server error message to
    internal server error detected instead of
    main error message

--- src/shared/errors/internal-sever-error.ts
+++ src/shared/errors/internal-sever-error.ts
@@ -5,7 +5,7 @@ export class InternalServerError extends SiteError {
     readonly status = StatusCode.ServerErrorInternal
 
     constructor(public readonly mainError: Error) {
-        super(mainError.message);
+        super('internal server error');
         console.error(mainError);
         // TODO add some functions to contact admin/programmer for fix this internal server error
     }
