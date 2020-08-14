5a6dacc17f556df517a178507b3a16d5fae28955
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-14 21:27:41 +0430

    hides internal server error messages from api
    before this commit the internal server error
    messages will show to user ( final user )
    but now the internal server error will show
    just 'internal server error detected'
    and if the NODE_ENV set to development
    it will show the error message and
    the rest of the error in side response.err
    object

--- src/routes/errorHandler.ts
+++ src/routes/errorHandler.ts
@@ -1,12 +1,14 @@
 import {Request, Response, NextFunction} from 'express';
-import {SiteError} from '@shared/errors';
+import {InternalServerError, SiteError} from '@shared/errors';
 import util from 'util';
 
 export default (isDev: boolean) => (err: SiteError, req: Request, res: Response, next: NextFunction) => {
     const responseObject: { [p: string]: any } = {msg: err.message};
-    // console.log(isDev);
-    if (isDev)
+    if (isDev) {
         responseObject.err = util.inspect(err);
+        if (err instanceof InternalServerError)
+            responseObject.msg = err.mainError.message;
+    }
     console.log(err)
     return res.status(err.status).json(err.sanitizeResponse(responseObject));
 };
