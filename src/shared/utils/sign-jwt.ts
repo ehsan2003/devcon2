5ca75b433ebd76e5006c316b9de16687ed67f1a8
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-16 00:09:23 +0430

    removes useless console.log from sign-jwt.ts utility

--- src/shared/utils/sign-jwt.ts
+++ src/shared/utils/sign-jwt.ts
@@ -5,8 +5,6 @@ import jwt from 'jsonwebtoken';
 import {extractProps} from "@shared/utils";
 
 export const signJwt = (user: IUserDoc) => {
-    console.log(user);
-    console.log(extractProps(user,'email','id'));
     const token = jwt.sign(extractProps(user.toObject(),'email','_id'), keys.secretOrKey, configurations.jwtTokenSingOptions);
     return `Bearer ${token}`;
 };
