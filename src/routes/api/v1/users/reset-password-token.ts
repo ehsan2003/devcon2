cf0929bb9e6163f151ec5dc597a55aebd3dfc3f5
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-16 00:10:05 +0430

    adds code to reset-password/token route

--- src/routes/api/v1/users/reset-password-token.ts
+++ src/routes/api/v1/users/reset-password-token.ts
@@ -1,22 +1,41 @@
-import {BaseController, Roles} from "@shared/utils";
+import {BaseController, hashPassword, verifyByToken} from "@shared/utils";
 import {RequestHandler} from "express";
-import {body, ValidationChain} from "express-validator";
+import {body, param, ValidationChain} from "express-validator";
+import {verificationTypes} from "@models/Verification";
+import {BadRequestError} from "@shared/errors";
 
-type localRequestHandler = RequestHandler<{token:string}, {msg:string}, {}, {}>
+type localRequestHandler = RequestHandler<{ token: string }, { msg: string }, { newPassword: string }, {}>
 
 class ResetPasswordToken extends BaseController<localRequestHandler> {
 
     readonly access = null;
-    readonly method = 'get';
+    readonly method = 'post';
     readonly path: string = '/:token';
     protected middleware: localRequestHandler[]
         = [
         (async (req, res, next) => {
-
+            const {token} = req.params;
+            const {newPassword} = req.body;
+            const verificationResult = await verifyByToken(token, verificationTypes.resetPassword);
+            if (!verificationResult)
+                throw new BadRequestError('invalid token');
+            verificationResult.password = await hashPassword(newPassword)
+            await verificationResult.save()
+            res.json({msg: 'success'})
         })
     ];
 
-    protected validator: ValidationChain[] = [];
+    protected validator: ValidationChain[] = [
+        body('newPassword')
+            .exists().withMessage('newPassword is required')
+            .isString().withMessage('newPassword is not a string')
+            .isLength({min: 8, max: 100}).withMessage('newPassword length should be between 8 and 100')
+            .matches(/\d/).withMessage('newPassword should contains number')
+            .matches(/\w/).withMessage('newPassword should contains letters')
+        , param('token')
+            .exists().withMessage('token is required')
+            .isString().withMessage('token is not a string')
+    ];
 
     constructor() {
         super();
