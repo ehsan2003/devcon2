4cb16972ebdc02d86e1c3609ab00b4b7d9408e3d
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-15 22:09:03 +0430

    adds verification routes to users main router

--- src/routes/api/v1/users/index.ts
+++ src/routes/api/v1/users/index.ts
@@ -2,11 +2,15 @@ import {Router} from 'express';
 import register from './register';
 import Current from './current';
 import Login from './login';
+import verifyToken from './verify-token';
+import verifySend from './verify-send';
 
 const router = Router();
 
 router.use('/register', register.getRouter());
 router.use('/login', Login.getRouter());
 router.use('/current', Current.getRouter());
+router.use('/verify/send', verifySend.getRouter());
+router.use('/verify/token', verifyToken.getRouter());
 
 export default router;
