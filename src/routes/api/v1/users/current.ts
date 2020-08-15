45c065c5a5101ff498cb0da71c97f0ad493b4f2d
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-15 23:24:48 +0430

    secures user info in users/current

--- src/routes/api/v1/users/current.ts
+++ src/routes/api/v1/users/current.ts
@@ -1,9 +1,9 @@
-import {BaseController, Roles} from "@shared/utils";
+import {BaseController, Roles, secureUserInfo} from "@shared/utils";
 import {RequestHandler} from "express";
 import {ValidationChain} from "express-validator";
 import {IUserDoc} from "@models/User";
 
-type localRequestHandler = RequestHandler<{}, { msg: string, result: IUserDoc }, {}, {}>
+type localRequestHandler = RequestHandler<{}, { msg: string, result: ReturnType<typeof secureUserInfo> }, {}, {}>
 
 class Current extends BaseController<localRequestHandler> {
 
@@ -13,7 +13,7 @@ class Current extends BaseController<localRequestHandler> {
     protected middleware: localRequestHandler[]
         = [
         (async (req, res, next) => {
-            res.json({msg: 'success', result: req.user as IUserDoc})
+            res.json({msg: 'success', result: secureUserInfo(req.user as IUserDoc)})
         })
     ];
 
