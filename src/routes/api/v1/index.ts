bcfd4d49e427af1d0126d1422cfd455039dd1a0c
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-16 18:52:51 +0430

    adds categories main router

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import users from './users';
 import tags from './tags';
+import categories from './categories';
 
 const router = Router();
 
 router.use('/users', users);
 router.use('/tags', tags);
+router.use('/categories', categories);
 
 export default router;
