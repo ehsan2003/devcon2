b606d4e79f49501e0c2ca89dc2d738e5d762c4f9
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-16 14:19:31 +0430

    adds Model.tagsExists method to Tag model

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -1,4 +1,4 @@
-import {Schema, model, Document, Model} from 'mongoose';
+import {Document, model, Model, Schema} from 'mongoose';
 
 export interface ITagDoc extends Document {
     slug: string;
@@ -22,8 +22,10 @@ const ModelSchema = new Schema({
 });
 ModelSchema.static('tagsExist', function (this: Model<ITagDoc>, tagsIds: string[]) {
     return new Promise(((resolve, reject) => this.find({_id: {$in: tagsIds}}).select('_id')
-        .then(tagsDocs =>
-            tagsDocs.length < tagsIds.length ? resolve(true) : resolve(false)
-        )))
+        .then(tagsDocs => {
+                console.log(tagsDocs, tagsIds);
+                tagsDocs.length == tagsIds.length ? resolve(true) : resolve(false)
+            }
+        ).catch(reject)))
 })
-export default model<ITagDoc>('tags', ModelSchema);
+export default model<ITagDoc, ITagModel>('tags', ModelSchema);
