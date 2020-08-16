7271ec5b9a96d7d71bc25a7973b4aff8adc1f960
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-16 12:28:21 +0430

    adds tags main route

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import users from './users';
+import tags from './tags';
 
 const router = Router();
 
 router.use('/users', users);
+router.use('/tags', tags);
 
 export default router;
