9b66827d2a70c03ebc84b590058d89e14032b698
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-17 12:18:02 +0430

    removes handle-unique function from utils
    that's because this function is now
    available in BaseController

--- src/shared/utils/handle-unique.ts
+++
@@ -1,9 +0,0 @@
-import {mongo} from "mongoose";
-import {ConflictError} from "@shared/errors";
-
-export const handleUnique = (message: string) => (err: Error) => {
-    if (err instanceof mongo.MongoError && err.code === 11000)
-        throw new ConflictError(message);
-    else
-        throw err;
-}
