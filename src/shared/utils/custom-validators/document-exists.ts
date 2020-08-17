a3432b9052602c524dda5c0629ff8a1bf555a2b6
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-17 12:09:46 +0430

    removes document-exists.ts because it's useless

--- src/shared/utils/custom-validators/document-exists.ts
+++
@@ -1,4 +0,0 @@
-import {Document, FilterQuery, Model} from "mongoose";
-
-export const documentExists = <T extends Document>(model: Model<T>, query: FilterQuery<T>) =>
-    model.exists(query);
--- src/shared/utils/custom-validators/index.ts
+++ src/shared/utils/custom-validators/index.ts
@@ -1,2 +1,1 @@
 export * from './is-valid-regexp';
-export * from './document-exists';
