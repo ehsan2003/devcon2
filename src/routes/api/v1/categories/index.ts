7da277d679c6b597f583ad1247c6e998699a3b3d
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-18 20:00:58 +0430

    adds categories/delete to main categories route

--- src/routes/api/v1/categories/index.ts
+++ src/routes/api/v1/categories/index.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import insert from './insert';
 import update from './update';
+import Delete from './delete';
 
 const router = Router();
 
 router.use('/insert', insert.getRouter());
 router.use('/update', update.getRouter());
+router.use('/delete', Delete.getRouter());
 
 export default router;
