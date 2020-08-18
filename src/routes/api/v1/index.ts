54b17bac0c49bbde97ec833587a361d5736563d4
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-18 20:12:31 +0430

    adds comments main router to main api router

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -2,9 +2,10 @@ import {Router} from 'express';
 import users from './users';
 import tags from './tags';
 import categories from './categories';
+import comments from './comments';
 
 const router = Router();
-
+router.use('/comments', comments);
 router.use('/users', users);
 router.use('/tags', tags);
 router.use('/categories', categories);
