0dfcd0e73f9811168cdf90db709aad77841d35e8
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-18 19:54:36 +0430

    adds categories/update to main categories router

--- src/routes/api/v1/categories/index.ts
+++ src/routes/api/v1/categories/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import insert from './insert';
+import update from './update';
 
 const router = Router();
 
 router.use('/insert', insert.getRouter());
+router.use('/update', update.getRouter());
 
 export default router;
