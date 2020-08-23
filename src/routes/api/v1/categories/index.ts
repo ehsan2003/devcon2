7517aaf84a24f69fb5b28b4808b1caca66f8c0ab
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-23 20:47:35 +0430

    adds categories/search route without validator

--- src/routes/api/v1/categories/index.ts
+++ src/routes/api/v1/categories/index.ts
@@ -4,6 +4,7 @@ import update from './update';
 import Delete from './delete';
 import all from './all';
 import changeParent from './change-parent';
+import search from './search';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/update', update.getRouter());
 router.use('/delete', Delete.getRouter());
 router.use('/all', all.getRouter());
 router.use('/change-parent', changeParent.getRouter());
+router.use('/search', search.getRouter());
 
 export default router;
---
+++ src/routes/api/v1/categories/search.ts
@@ -0,0 +1,35 @@
+import {BaseController} from "@shared/utils";
+import {RequestHandler} from "express";
+import {ValidationChain} from "express-validator";
+import configurations from "@conf/configurations";
+import Category, {ICategoryDoc} from "@models/Category";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: ICategoryDoc[] }, {}, { q: string, l?: string }>;
+
+class Search extends BaseController<localRequestHandler> {
+
+    readonly access = null;
+    readonly method = 'get';
+    readonly path: string = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        (async (req, res, next) => {
+            const defaultLimit = configurations.categories.search.limit;
+            const result = await Category.find({$or: [{slug: {$regex: req.query.q}}, {enName: {$regex: req.query.q}}]}).limit(parseInt(req.query.l || '0', 10) || defaultLimit);
+            if (!result)
+                throw new NotFoundError('categories not found');
+            res.json({msg: 'success', result});
+
+        })
+    ];
+
+    protected validator: ValidationChain[] = [];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Search();
