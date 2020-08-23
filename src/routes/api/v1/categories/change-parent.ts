d9478906244779c0408853b9c7d0d7ce9feac006
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-23 20:36:33 +0430

    adds categories/change-parent route and validator

---
+++ src/routes/api/v1/categories/change-parent.ts
@@ -0,0 +1,41 @@
+import {BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {body, ValidationChain} from "express-validator";
+import {Types} from "mongoose";
+import Category from "@models/Category";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: any }, { source: Types.ObjectId, dest?: Types.ObjectId }, {}>
+
+class ChangeParent extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.editor;
+    readonly method = 'put';
+    readonly path: string = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        (async (req, res, next) => {
+            const result = await Category.updateMany({parent: req.body.source}, {$set: {parent: req.body.dest || null}});
+            res.json({msg: 'success', result});
+        })
+    ];
+
+    protected validator: ValidationChain[] = [
+        body('source')
+            .exists().withMessage('required')
+            .isMongoId().withMessage('invalid mongo id')
+            .customSanitizer(Types.ObjectId)
+            .custom(dest => Category.exists({_id: dest})).withMessage('not found')
+        , body('dest')
+            .optional()
+            .isMongoId().withMessage('invalid mongo id')
+            .customSanitizer(Types.ObjectId)
+            .custom(dest => Category.exists({_id: dest})).withMessage('not found')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new ChangeParent();
--- src/routes/api/v1/categories/index.ts
+++ src/routes/api/v1/categories/index.ts
@@ -3,6 +3,7 @@ import insert from './insert';
 import update from './update';
 import Delete from './delete';
 import all from './all';
+import changeParent from './change-parent';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/insert', insert.getRouter());
 router.use('/update', update.getRouter());
 router.use('/delete', Delete.getRouter());
 router.use('/all', all.getRouter());
+router.use('/change-parent', changeParent.getRouter());
 
 export default router;
