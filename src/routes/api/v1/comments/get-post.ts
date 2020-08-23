edc3429bbbf759a72460239fcfecdca4b297fdba
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-23 16:09:49 +0430

    adds comments/get-post route

---
+++ src/routes/api/v1/comments/get-post.ts
@@ -0,0 +1,45 @@
+import {BaseController} from "@shared/utils";
+import {RequestHandler} from "express";
+import {ValidationChain} from "express-validator";
+import Comment, {ICommentDoc} from "@models/Comment";
+import {Types} from "mongoose";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: ICommentDoc[] }, {}, {}>
+
+class GetPost extends BaseController<localRequestHandler> {
+
+    readonly access = null;
+    readonly method = 'get';
+    readonly path: string = '/:id';
+    protected middleware: localRequestHandler[]
+        = [
+        (async (req, res, next) => {
+            const result = await Comment.aggregate().match({
+                visible: true,
+                forPost: Types.ObjectId(req.params.id),
+                responseTo: null
+            }).lookup({
+                from:'comments',
+                localField:'_id',
+                foreignField:'responseTo',
+                as:'responses'
+            }).addFields({
+                response:{$size:'$responses'}
+            });
+            if (!result.length)
+                throw new NotFoundError('comments not found');
+            res.json({msg: 'success', result});
+
+        })
+    ];
+
+    protected validator: ValidationChain[] = [];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new GetPost();
--- src/routes/api/v1/comments/index.ts
+++ src/routes/api/v1/comments/index.ts
@@ -4,6 +4,7 @@ import insertUnAuthorized from './insert-unauthorized'
 import remove from './remove';
 import visible from './visible';
 import update from './update';
+import getPost from './get-post';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/insert', insertAuthorized.getRouter(), insertUnAuthorized.getRouter
 router.use('/remove', remove.getRouter());
 router.use('/visible', visible.getRouter());
 router.use('/update', update.getRouter());
+router.use('/get-post', getPost.getRouter());
 
 export default router;
