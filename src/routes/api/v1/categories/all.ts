45e1e8e57cf3079b7104c477a5494fe0685dce7f
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-23 20:16:30 +0430

    adds categories/all route

---
+++ src/routes/api/v1/categories/all.ts
@@ -0,0 +1,25 @@
+import {BaseController} from "@shared/utils";
+import {RequestHandler} from "express";
+import Category, {ICategoryDoc} from "@models/Category";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: ICategoryDoc[] }, {}, {}>;
+
+class All extends BaseController<localRequestHandler> {
+
+    readonly access = null;
+    readonly method = 'get';
+    readonly path: string = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        (async (req, res, next) => {
+            res.json({msg: 'success', result: await Category.find({})});
+        })
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new All();
--- src/routes/api/v1/categories/index.ts
+++ src/routes/api/v1/categories/index.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import insert from './insert';
 import update from './update';
 import Delete from './delete';
+import all from './all';
 
 const router = Router();
 
 router.use('/insert', insert.getRouter());
 router.use('/update', update.getRouter());
 router.use('/delete', Delete.getRouter());
+router.use('/all', all.getRouter());
 
 export default router;
