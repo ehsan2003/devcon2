5ff1fba38048b3ebfb41de96f9139b7cc63e0a66
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-24 21:28:46 +0430

    adds posts main router

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -3,11 +3,13 @@ import users from './users';
 import tags from './tags';
 import categories from './categories';
 import comments from './comments';
+import posts from './posts';
 
 const router = Router();
 router.use('/comments', comments);
 router.use('/users', users);
 router.use('/tags', tags);
 router.use('/categories', categories);
+router.use('/posts', posts);
 
 export default router;
