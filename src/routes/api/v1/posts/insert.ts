c74a06a2935b0bd81177a1c75e804f08a1ebd6e5
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-24 21:44:40 +0430

    adds posts/insert route without validator

---
+++ src/routes/api/v1/posts/insert.ts
@@ -0,0 +1,34 @@
+import {BaseController, extractProps, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {ValidationChain} from "express-validator";
+import Post, {IPostDoc} from "@models/Post";
+import {IUserDoc} from "@models/User";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: IPostDoc }, Pick<IPostDoc, 'content' | 'title' | 'slug' | 'featuredImage' | 'category' | 'tags'>, {}>;
+
+class Insert extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.contributor;
+    readonly method = 'post';
+    readonly path: string = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        (async (req, res, next) => {
+            const result = new Post({
+                    ...extractProps(req.body, 'content', 'title', 'slug', 'featuredImage', 'category', 'tags'),
+                    author: (req.user as IUserDoc)._id
+                }
+            );
+            res.json({msg: 'success', result});
+        })
+    ];
+
+    protected validator: ValidationChain[] = [];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Insert();
