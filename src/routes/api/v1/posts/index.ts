50b5b25156f0f602bcaa523396630566ca100da4
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-26 00:17:32 +0430

    adds posts/visible route to main router

--- src/routes/api/v1/posts/index.ts
+++ src/routes/api/v1/posts/index.ts
@@ -8,5 +8,6 @@ const router = Router();
 router.use('/insert', insert.getRouter());
 router.use('/update', update.getRouter());
 router.use('/delete', Delete.getRouter());
-
+import visible from './visible';
+router.use('/visible',visible.getRouter());
 export default router;
