d65493838894da5eea7624e37503d10277792ad8
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-26 12:13:07 +0430

    adds posts/like route and validator

--- src/routes/api/v1/posts/index.ts
+++ src/routes/api/v1/posts/index.ts
@@ -4,6 +4,7 @@ import update from './update';
 import Delete from './delete';
 import visible from './visible';
 import get from './get';
+import like from './like';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/update', update.getRouter());
 router.use('/delete', Delete.getRouter());
 router.use('/visible', visible.getRouter());
 router.use('/get', get.getRouter());
+router.use('/like', like.getRouter());
 
 export default router;
---
+++ src/routes/api/v1/posts/like.ts
@@ -0,0 +1,42 @@
+import {BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import Post from "@models/Post";
+import {IUserDoc} from "@models/User";
+import {Types} from "mongoose";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: any }, {}, {}>;
+
+class Like extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.subscriber;
+    readonly method = 'put';
+    readonly path = '/:id';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const id = Types.ObjectId(req.params.id);
+            const result = await Post.updateOne({_id: id}, {$addToSet: {likes: (req.user as IUserDoc).id}});
+            if (result.nMatched === 0)
+                throw new NotFoundError('post not found');
+
+            const updatedPost = await Post.mapLikesToNumber({_id: id});
+
+            res.json({msg: 'success', result: updatedPost});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('id')
+            .exists().withMessage('required')
+            .isMongoId().withMessage('invalid mongo id')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Like();
