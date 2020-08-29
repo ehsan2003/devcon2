ecd2e497ae7cb9b67f87216533748e43d1b8bf03
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-29 20:54:40 +0430

    adds posts/search/:category route without validator

--- src/routes/api/v1/posts/index.ts
+++ src/routes/api/v1/posts/index.ts
@@ -6,6 +6,7 @@ import visible from './visible';
 import get from './get';
 import like from './like';
 import dislike from './dislike';
+import search from './search-category';
 
 const router = Router();
 
@@ -16,5 +17,5 @@ router.use('/visible', visible.getRouter());
 router.use('/get', get.getRouter());
 router.use('/like', like.getRouter());
 router.use('/dislike', dislike.getRouter());
-
+router.use('/search', search.getRouter());
 export default router;
---
+++ src/routes/api/v1/posts/search-category.ts
@@ -0,0 +1,90 @@
+import {AggregationChain, BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {ValidationChain} from "express-validator";
+import Category from "@models/Category";
+import configurations from "@conf/configurations";
+import {NotFoundError} from "@shared/errors";
+import {IPostDocSharable} from "@models/Post";
+
+type localRequestHandler = RequestHandler<{ category: string }, { msg: string, result: (IPostDocSharable & { isLiked?: boolean })[] }, {}, {
+    q?: string;
+    s?: 'd' | 'l';
+    o?: 'a' | 'd';
+    k?: string;
+    l?: string;
+}>;
+
+class SearchByCategory extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.anonymous;
+    readonly method = 'get';
+    readonly path = '/:category';
+
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const queryString = req.query.q;
+            const sortBy = req.query.s;
+            const sortOrder = req.query.o;
+            const category = req.params.category;
+            const searchLimit = parseInt(req.query.l || configurations.posts.search.limit.toString(), 10);
+            const searchSkip = parseInt(req.query.k || '0', 10);
+            const postQuery = {
+                visible: true,
+                ...queryString ? {$or: [{title: {$regex: queryString}}, {content: {$regex: queryString}}]} : []
+            };
+            const chain = new AggregationChain()
+                .match({slug: category})
+                .graphLookup({
+                    from: 'categories',
+                    startWith: '$_id',
+                    connectFromField: '_id',
+                    connectToField: 'parent',
+                    as: 'children'
+                })
+                .project({
+                    children: {
+                        $concatArrays: [
+                            '$children', [
+                                {
+                                    _id: '$_id'
+                                }
+                            ]
+                        ]
+                    }
+                })
+                .project({'children._id': 1})
+                .replaceUnwind('$children')
+                .lookup({
+                    as: 'posts'
+                    , foreignField: "category"
+                    , from: "posts"
+                    , localField: "_id"
+                })
+                .replaceUnwind('$posts')
+                .match(postQuery)
+                .addFields({isLiked: {$in: [req.user && req.user._id, '$likes']}}, !!req.user)
+                .addFields({likes: {$size: '$likes'}})
+                .sort({
+                    [sortBy === 'd' ? 'lastModified' : 'likes']: sortOrder === 'a' ? 1 : -1
+                })
+                .skip(searchSkip)
+                .limit(searchLimit);
+            console.log(JSON.stringify(chain.getPipelineInstance(), null, 10));
+
+            const result = await Category.aggregate(chain.getPipelineInstance());
+            if (!result.length)
+                throw new NotFoundError('post not found');
+            res.json({msg: 'success', result});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new SearchByCategory();
