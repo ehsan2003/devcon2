4fa80f9b4024095bc3d677c89d037b80bc248a44
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-30 20:43:30 +0430

    adds error logger to bin file

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -8,6 +8,14 @@ console.log(process.version);
 dotenv.config();
 if (process.env.NODE_ENV === 'development')
     process.env.IS_DEV = 'true';
+process.on('unhandledRejection', (err) => {
+    logger.error('unhandled promise rejection', {err});
+    console.error(err);
+});
+process.on('uncaughtException', (err) => {
+    logger.error('uncaughtException', {err});
+    console.error(err);
+});
 
 const PORT = process.env.PORT || '3001';
 app.listen(PORT, (err: Error) => {
