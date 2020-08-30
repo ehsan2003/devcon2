cbbcdaef703d449339130da6f602e0dda26430e1
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-08-30 12:43:25 +0430

    adds profiles main router

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -4,6 +4,7 @@ import tags from './tags';
 import categories from './categories';
 import comments from './comments';
 import posts from './posts';
+import profile from './profiles';
 
 const router = Router();
 router.use('/comments', comments);
@@ -11,5 +12,6 @@ router.use('/users', users);
 router.use('/tags', tags);
 router.use('/categories', categories);
 router.use('/posts', posts);
+router.use('/profiles', profile);
 
 export default router;
