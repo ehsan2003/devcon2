4153a191efa3d63461fd477d4c440dddbd968fd0
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-02 01:09:54 +0430

    adds upload main route to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import errorHandler from "./errorHandler";
 import errorLogger from "./errorLogger";
 import {NotFoundError} from "@shared/errors";
 import v1 from './api/v1';
+import upload from './upload';
 
 const router = Router();
 
 router.use('/api/v1', v1);
+router.use('/upload', upload);
 
 router.all('*', (req, res, next) => next(new NotFoundError(`route not found ${req.method} ${req.path}`)));
 router.use(errorLogger(process.env.NODE_ENV === 'development'));
