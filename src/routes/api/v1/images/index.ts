b4b0a4acf2fe3e11794a4c751726c20d88a92a5a
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-08 01:15:01 +0430

    adds v1/images/remove route and validator

--- src/routes/api/v1/images/index.ts
+++ src/routes/api/v1/images/index.ts
@@ -2,10 +2,13 @@ import {Router} from 'express';
 import changeSlug from './change-slug';
 import changeAccess from './change-access';
 import data from './data';
+import remove from './remove';
 
 const router = Router();
 
 router.use('/change-slug', changeSlug.getRouter());
 router.use('/change-access', changeAccess.getRouter());
 router.use('/data', data.getRouter());
+router.use('/remove', remove.getRouter());
+
 export default router;
---
+++ src/routes/api/v1/images/remove.ts
@@ -0,0 +1,36 @@
+import {BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import ImageData, {IImageDataDoc} from "@models/ImageData";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: IImageDataDoc }, {}, {}>;
+
+class Remove extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.editor;
+    readonly method = 'delete';
+    readonly path = '/:id';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const imageData = await ImageData.findById(req.params.id);
+            if (!imageData)
+                throw new NotFoundError('image not found');
+            await imageData.removeAll();
+            res.json({msg: 'success', result: imageData});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('id')
+            .isMongoId().withMessage('invalid mongo id')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Remove();
