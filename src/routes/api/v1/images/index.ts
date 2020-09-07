b482adc40c0666c4cd7a85ae7050d701b96d8588
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-08 00:59:17 +0430

    modifies /api/v1/images main route

--- src/routes/api/v1/images/index.ts
+++ src/routes/api/v1/images/index.ts
@@ -1,7 +1,10 @@
 import {Router} from 'express';
+import changeSlug from './change-slug';
+import changeAccess from './change-access';
 
 const router = Router();
 
-
+router.use('/change-slug', changeSlug.getRouter());
+router.use('/change-access', changeAccess.getRouter());
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,16 @@
-import {Router} from 'express';
+import express, {Router} from 'express';
 import errorHandler from "./errorHandler";
 import errorLogger from "./errorLogger";
 import {NotFoundError} from "@shared/errors";
 import v1 from './api/v1';
 import upload from './upload';
+import fileProtection from './file-protection';
 
 const router = Router();
 
 router.use('/api/v1', v1);
 router.use('/upload', upload);
-
+router.use('/static', fileProtection.getRouter(), express.static('upload'));
 router.all('*', (req, res, next) => next(new NotFoundError(`route not found ${req.method} ${req.path}`)));
 router.use(errorLogger(process.env.NODE_ENV === 'development'));
 router.use(errorHandler(process.env.NODE_ENV === 'development'));
