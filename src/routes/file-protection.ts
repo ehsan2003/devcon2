3526513718bc4c32678780e65c333c625abe11fe
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-08 01:01:08 +0430

    adds file-protection router and validator

---
+++ src/routes/file-protection.ts
@@ -0,0 +1,38 @@
+import {BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import {IUserDoc} from "@models/User";
+import {AccessForbiddenError} from "@shared/errors";
+
+type roleStrings = 'subscriber' | 'administrator' | 'contributor' | 'editor' | 'superAdmin' | 'unverified';
+type localRequestHandler = RequestHandler<{ role: roleStrings }, { msg: string }, {}, {}>;
+
+class FileProtection extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.unverified;
+    readonly method = 'get';
+    readonly path = '/protected/:role/*';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const accessRole = Roles[req.params.role];
+            const user = req.user as IUserDoc;
+            if (user.role < accessRole)
+                throw new AccessForbiddenError('file is not accessible');
+            next();
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('role')
+            .exists().withMessage('required')
+            .isIn(['subscriber', 'administrator', 'contributor', 'editor', 'superAdmin', 'unverified'])
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new FileProtection();
