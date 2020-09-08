eeae0deb2abe7bee54f7a393fc8ea70343e893ef
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-08 20:38:14 +0430

    adds images/search route and validator

--- src/routes/api/v1/images/index.ts
+++ src/routes/api/v1/images/index.ts
@@ -3,6 +3,7 @@ import changeSlug from './change-slug';
 import changeAccess from './change-access';
 import data from './data';
 import remove from './remove';
+import search from './search';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/change-slug', changeSlug.getRouter());
 router.use('/change-access', changeAccess.getRouter());
 router.use('/data', data.getRouter());
 router.use('/remove', remove.getRouter());
+router.use('/search', search.getRouter());
 
 export default router;
---
+++ src/routes/api/v1/images/search.ts
@@ -0,0 +1,66 @@
+import {AggregationChain, BaseController, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {query, ValidationChain} from "express-validator";
+import ImageData, {IImageDataDoc} from "@models/ImageData";
+import configurations from "@conf/configurations";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: IImageDataDoc[] }, {},
+    {
+        search?: string, access?: Roles, mimetype?: string, sortOrder: 'a' | 'd', sortBy: 'd' | 's' | 'm' | 'r'
+    }>;
+
+class Search extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.contributor;
+    readonly method = 'get';
+    readonly path = '/';
+    readonly sortMethods = {
+        d: 'dateModified',
+        s: 'sizes.full.fileSize',
+        m: 'mimetype',
+        r: 'score'
+    };
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const reqQuery = req.query;
+            const matchQuery = {
+                ...reqQuery.search ? {$text: {$search: reqQuery.search}} : {},
+                ...reqQuery.mimetype ? {mimetype: reqQuery.mimetype} : {},
+                ...reqQuery.access ? {access: reqQuery.access} : {},
+            };
+            const aggregationChain = new AggregationChain()
+                .match(matchQuery)
+                .addFields({score: {$meta: 'textScore'}}, !!reqQuery.search)
+                .sort({
+                    [this.sortMethods[reqQuery.sortBy]]: reqQuery.sortOrder !== 'a' ? -1 : 1
+                });
+            res.json({msg: 'success', result: await aggregationChain.run(ImageData)});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        query('search')
+            .optional()
+            .isString().withMessage('invalid string'),
+        query('access')
+            .optional()
+            .isIn([-1, 0, 1, 2, 3, 4, 5, 6])
+        , query('mimetype')
+            .optional()
+            .isIn(configurations.posts.image.allowedMimeTypes).withMessage('invalid mimetype')
+        , query('sortOrder')
+            .optional()
+            .isIn(['a', 'd'])
+        , query('sortBy')
+            .optional()
+            .isIn(['d', 's', 'm', 'r'])
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Search();
