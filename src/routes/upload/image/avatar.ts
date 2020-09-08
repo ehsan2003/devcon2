10c3f0813b7ebed42a5860acee6c8fb64e5b3d95
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-08 22:21:36 +0430

    adds /upload/image/avatar route ( no validator required )

---
+++ src/routes/upload/image/avatar.ts
@@ -0,0 +1,59 @@
+import {Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {IImageDataDoc} from "@models/ImageData";
+import {ImageUploader} from './controller-base';
+import multer from "multer";
+import configurations from "@conf/configurations";
+import {IUserDoc} from "@models/User";
+import Profile, {IProfileDoc} from "@models/Profile";
+import {NotFoundError} from "@shared/errors";
+import path from 'path';
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: IImageDataDoc }, {}, {}>;
+
+class Avatar extends ImageUploader<localRequestHandler> {
+    readonly access = Roles.subscriber;
+    readonly method = 'post';
+    readonly path = '/';
+    protected upload = multer({
+        fileFilter(req, file: Express.Multer.File, callback: multer.FileFilterCallback): void {
+            if (!configurations.profile.avatar.allowedMimeTypes.includes(file.mimetype))
+                callback(new multer.MulterError('LIMIT_UNEXPECTED_FILE'));
+            callback(null, true);
+        },
+        limits: configurations.profile.avatar.uploadLimit,
+        storage: multer.memoryStorage()
+    });
+    protected middleware: localRequestHandler[]
+        = [this.upload.single('avatar'),
+        async (req, res, next) => {
+            const user = req.user as IUserDoc;
+            const profile = await Profile.findOne({user: user._id});
+            if (!profile)
+                throw new NotFoundError('profile not found');
+            req.data = profile;
+            next();
+
+        },
+        async (req, res, next) => {
+            const user = req.user as IUserDoc;
+            const profile = req.data as IProfileDoc;
+            const result = await this.saveImage({
+                buff: req.file.buffer,
+                access: Roles.anonymous,
+                mimetype: req.file.mimetype,
+                slugPrefix: path.join('avatar', profile.slug),
+                info: {uploader: user._id, alt: profile.firstName, title: profile.firstName},
+                sizes: configurations.profile.avatar.sizes
+            });
+            res.json({msg: 'success', result});
+        }
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Avatar();
--- src/routes/upload/image/index.ts
+++ src/routes/upload/image/index.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express';
 import post from './post';
+import avatar from './avatar';
 
 const router = Router();
 router.use('/post', post.getRouter());
+router.use('/avatar', avatar.getRouter());
 
 export default router;
