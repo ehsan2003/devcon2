e9732df56e20995272239026c2bc17fa60262ba8
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-09 21:12:58 +0430

    adds /upload/image/post-update route

--- src/routes/upload/image/index.ts
+++ src/routes/upload/image/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import post from './post';
 import avatar from './avatar';
+import postUpdate from './post-update';
 
 const router = Router();
 router.use('/post', post.getRouter());
 router.use('/avatar', avatar.getRouter());
+router.use('/post-update', postUpdate.getRouter());
 
 export default router;
---
+++ src/routes/upload/image/post-update.ts
@@ -0,0 +1,53 @@
+import {Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import ImageData, {IImageDataDoc} from "@models/ImageData";
+import {ImageUploader} from "./controller-base";
+import multer from "multer";
+import configurations from "@conf/configurations";
+import {BadRequestError, NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: IImageDataDoc }, {}, {}>;
+
+class PostUpdate extends ImageUploader<localRequestHandler> {
+
+    readonly access = Roles.editor;
+    readonly method = 'put';
+    readonly path = '/:id';
+    protected upload = multer({
+        fileFilter(req, file: Express.Multer.File, callback: multer.FileFilterCallback): void {
+            if (!configurations.posts.image.allowedMimeTypes.includes(file.mimetype))
+                callback(new multer.MulterError('LIMIT_UNEXPECTED_FILE'));
+            callback(null, true);
+        },
+        limits: configurations.posts.image.uploadLimit,
+        storage: multer.memoryStorage()
+    });
+    protected middleware: localRequestHandler[]
+        = [this.upload.single('image'),
+        (req, res, next) => {
+            if (!req.file)
+                throw new BadRequestError('no file');
+            return next();
+        },
+        async (req, res, next) => {
+            const imageDataDoc = await ImageData.findById(req.params.id);
+            if (!imageDataDoc)
+                throw new NotFoundError('imageData not found');
+            await this.saveImageFiles({imageDataDoc, sizes: configurations.posts.image.sizes, buffer: req.file.buffer});
+            res.json({msg: 'success', result: imageDataDoc});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('id')
+            .isMongoId().withMessage('invalid mongo id')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new PostUpdate();
