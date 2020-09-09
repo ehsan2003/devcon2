76776a06664f756d606fb459b3c72d070f14f16f
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-10 00:39:30 +0430

    adds users/change-role route and validator

---
+++ src/routes/api/v1/users/change-rols.ts
@@ -0,0 +1,62 @@
+import {BaseController, Roles, secureUserInfo} from "@shared/utils";
+import {RequestHandler} from "express";
+import {body, param, ValidationChain} from "express-validator";
+import User, {IUserDoc} from "@models/User";
+import {AccessForbiddenError, NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: ReturnType<typeof secureUserInfo> }, {
+    newRole: Roles;
+}, {}>;
+
+class ChangeRoles extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.editor;
+    readonly method = 'put';
+    readonly path = '/:id';
+    readonly allowedChanges = {
+        [Roles.anonymous]: -2,
+        [Roles.unverified]: -2,
+        [Roles.subscriber]: -2,
+        [Roles.contributor]: -2,
+        [Roles.author]: -2,
+        [Roles.editor]: Roles.author,
+        [Roles.administrator]: Roles.editor,
+        [Roles.superAdmin]: Roles.administrator
+    };
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const user = req.user as IUserDoc;
+            if (this.allowedChanges[user.role] < req.body.newRole)
+                throw new AccessForbiddenError('access forbidden');
+            const modifiedUser = await User.findById(req.params.id);
+            if (!modifiedUser)
+                throw new NotFoundError('user not found');
+            res.json({msg: 'success', result: secureUserInfo(modifiedUser)});
+        }
+
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('id')
+            .exists().withMessage('required')
+            .isMongoId().withMessage('invalid mongo id'),
+        body('newRole')
+            .exists().withMessage('required')
+            .isIn([
+                Roles.subscriber,
+                Roles.contributor,
+                Roles.author,
+                Roles.editor,
+                Roles.administrator,
+                Roles.superAdmin
+            ])
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new ChangeRoles();
--- src/routes/api/v1/users/index.ts
+++ src/routes/api/v1/users/index.ts
@@ -7,6 +7,7 @@ import verifySend from './verify-send';
 import resetPasswordSend from './reset-password-send';
 import resetPasswordToken from './reset-password-token';
 import changePassword from './change-password';
+import ChangeRole from './change-rols';
 
 const router = Router();
 
@@ -18,5 +19,6 @@ router.use('/verify/token', verifyToken.getRouter());
 router.use('/reset-password/send', resetPasswordSend.getRouter());
 router.use('/reset-password/token', resetPasswordToken.getRouter());
 router.use('/change-password', changePassword.getRouter());
+router.use('/change-role', ChangeRole.getRouter());
 
 export default router;
