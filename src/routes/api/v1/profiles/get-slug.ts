3841d67145cbc07167407fa3304373f5060b3cca
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-09 21:18:31 +0430

    adds profiles/get-slug route and validator

---
+++ src/routes/api/v1/profiles/get-slug.ts
@@ -0,0 +1,36 @@
+import {BaseController} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import Profile, {IProfileDoc} from "@models/Profile";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ slug: string }, { msg: string, result: IProfileDoc }, {}, {}>;
+
+class GetSlug extends BaseController<localRequestHandler> {
+
+    readonly access = null;
+    readonly method = 'get';
+    readonly path = '/:slug';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const profile = await Profile.findOne({slug: req.params.slug});
+            if (!profile)
+                throw new NotFoundError('profile not found');
+            res.json({msg: 'success', result: profile});
+
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('slug')
+            .isSlug().withMessage('invalid input')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new GetSlug();
--- src/routes/api/v1/profiles/index.ts
+++ src/routes/api/v1/profiles/index.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express';
 import create from './create';
+import getSlug from './get-slug';
 
 const router = Router();
 router.use('/create', create.getRouter());
-
+router.use('/get-slug', getSlug.getRouter());
 
 export default router;
