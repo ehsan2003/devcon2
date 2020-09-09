94b9534336a8826b1ea2aa5e9136466e5c5405fe
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-09 21:21:20 +0430

    adds profiles/get-id route and validator

---
+++ src/routes/api/v1/profiles/get-id.ts
@@ -0,0 +1,36 @@
+import {BaseController} from "@shared/utils";
+import {RequestHandler} from "express";
+import {param, ValidationChain} from "express-validator";
+import Profile, {IProfileDoc} from "@models/Profile";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{ id: string }, { msg: string, result: IProfileDoc }, {}, {}>;
+
+class GetId extends BaseController<localRequestHandler> {
+
+    readonly access = null;
+    readonly method = 'get';
+    readonly path = '/:id';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const profile = await Profile.findById(req.params.id);
+            if (!profile)
+                throw new NotFoundError('profile not found');
+            res.json({msg: 'success', result: profile});
+
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        param('id')
+            .isMongoId().withMessage('invalid mongo id')
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new GetId();
--- src/routes/api/v1/profiles/index.ts
+++ src/routes/api/v1/profiles/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import create from './create';
 import getSlug from './get-slug';
+import getId from './get-id';
 
 const router = Router();
 router.use('/create', create.getRouter());
 router.use('/get-slug', getSlug.getRouter());
+router.use('/get-id', getId.getRouter());
 
 export default router;
