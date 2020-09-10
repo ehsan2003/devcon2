08f4fb45326713b6f2be7d1f94bb5876dcbec74d
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-10 21:07:35 +0430

    adds profile/update route and validator

--- src/routes/api/v1/profiles/index.ts
+++ src/routes/api/v1/profiles/index.ts
@@ -2,10 +2,11 @@ import {Router} from 'express';
 import create from './create';
 import getSlug from './get-slug';
 import getId from './get-id';
+import update from './update';
 
 const router = Router();
 router.use('/create', create.getRouter());
 router.use('/get-slug', getSlug.getRouter());
 router.use('/get-id', getId.getRouter());
-
+router.use('/update', update.getRouter());
 export default router;
---
+++ src/routes/api/v1/profiles/update.ts
@@ -0,0 +1,49 @@
+import {BaseController, extractProps, Roles} from "@shared/utils";
+import {RequestHandler} from "express";
+import {body, ValidationChain} from "express-validator";
+import Profile, {IProfileDoc} from "@models/Profile";
+import {IUserDoc} from "@models/User";
+import {NotFoundError} from "@shared/errors";
+
+type localRequestHandler = RequestHandler<{}, { msg: string, result: IProfileDoc }, Partial<Pick<IProfileDoc, 'firstName' | 'lastName' | 'social' | 'bio'>>, {}>;
+
+class Update extends BaseController<localRequestHandler> {
+
+    readonly access = Roles.subscriber;
+    readonly method = 'put';
+    readonly path = '/';
+    protected middleware: localRequestHandler[]
+        = [
+        async (req, res, next) => {
+            const user = req.user as IUserDoc;
+            const profile = await Profile.findOneAndUpdate({user: user._id}, {$set: extractProps(req.body, 'firstName', 'lastName', 'social', 'bio')});
+            if (!profile)
+                throw new NotFoundError('profile not found');
+            res.json({msg: 'success', result: profile});
+        }
+    ];
+
+    protected validator: ValidationChain[] = [
+        body('firstName')
+            .optional()
+            .isString().withMessage('invalid string')
+            .isLength({min: 2, max: 20}).withMessage('invalid length')
+        , body('lastName')
+            .optional()
+            .isString().withMessage('invalid string')
+            .isLength({min: 2, max: 20}).withMessage('invalid length')
+        , body('social.*')
+            .isURL().withMessage('invalid url')
+        , body('bio')
+            .optional()
+            .isString().withMessage('invalid string')
+            .isLength({min: 10, max: 500})
+    ];
+
+    constructor() {
+        super();
+        this.initialize();
+    }
+}
+
+export default new Update();
