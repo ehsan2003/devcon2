faa9fa21b37eabb73581e2d86ea2b544d01b9a13
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-18 14:02:59 +0430

    removes FETCH_CATEGORIES creator type

--- client/src/actions/creator-fetch-categories.ts
+++ client/src/actions/creator-fetch-categories.ts
@@ -1,11 +1,10 @@
 import {BaseAction} from "./base-action";
-import type {ActionCreator} from 'redux';
 
 export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
 
 export interface ActionTypeFetchCategories extends BaseAction<typeof FETCH_CATEGORIES> {
 }
 
-export const fetchCategories: ActionCreator<ActionTypeFetchCategories> = () => ({
+export const fetchCategories = () => ({
     type: FETCH_CATEGORIES
 });
