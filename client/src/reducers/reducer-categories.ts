bcdeaaac182abacda7c19758d21459abd35217c5
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-25 22:57:45 +0330

    modifies categories reducer which makes it prettier

--- client/src/reducers/reducer-categories.ts
+++ client/src/reducers/reducer-categories.ts
@@ -1,14 +1,19 @@
-import {Reducer} from 'redux';
-import {ICategory} from "../../../src/models/Category";
+import {Reducer} from "redux";
 import {AllActionTypes} from "../actions/base-action";
-import {CATEGORIES_FETCH} from "../actions/creator-categories-fetch";
-import {CATEGORIES_FETCH_FULFILLED} from "../actions/creator-categories-fetch-fulfilled";
+import {ICategory} from "../../../src/models/Category";
 
-export default (((state, action) => {
-    switch (action.type) {
-        case CATEGORIES_FETCH:
-            return null;
-        case CATEGORIES_FETCH_FULFILLED:
-            return action.payload;
-    }
-}) as Reducer<(ICategory & { _id: string })[] | null, AllActionTypes>);
+export type StateCategories = (ICategory<string> & { _id: string })[] | undefined;
+
+const categories: Reducer<StateCategories, AllActionTypes> =
+    (state, action) => {
+        switch (action.type) {
+            case "CATEGORIES_FETCH_FULFILLED":
+                return action.payload;
+            case "CATEGORIES_FETCH":
+                return undefined;
+            default:
+                return state;
+        }
+    };
+
+export default categories;
