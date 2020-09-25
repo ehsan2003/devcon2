37b5eaac73e24a7be6a4e7187906b97949734ae1
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-25 22:53:51 +0330

    modifies authorization token reducer which makes it prettier

--- client/src/reducers/reducer-authorization-token.ts
+++ client/src/reducers/reducer-authorization-token.ts
@@ -1,12 +1,16 @@
 import {Reducer} from "redux";
 import {AllActionTypes} from "../actions/base-action";
-import {LOGIN_RESPONDED} from "../actions/creator-login-responded";
 
-export default ((state, action) => {
-    switch (action.type) {
-        case LOGIN_RESPONDED:
-            return action.payload.token;
-        default:
-            return state;
-    }
-}) as Reducer<string, AllActionTypes>;
+export type StateAuthorizationToken = string | undefined;
+
+const authorizationToken: Reducer<StateAuthorizationToken, AllActionTypes> =
+    (state, action) => {
+        switch (action.type) {
+            case "LOGIN_RESPONDED":
+                return action.payload.token;
+            default:
+                return state;
+        }
+    };
+
+export default authorizationToken;
