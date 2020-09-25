eb28f15a6df05f7f7bd622a6739b11f16a1d4920
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-25 21:06:13 +0330

    adds user and authorization token reducers to root reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,6 +1,10 @@
 import {combineReducers} from "redux";
 import categories from './reducer-categories';
+import authorizationToken from './reducer-authorization-token';
+import user from './reducer-user';
 
 export default combineReducers({
-    categories
+    categories,
+    user,
+    authorizationToken
 });
