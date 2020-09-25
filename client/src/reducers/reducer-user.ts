26b1aa9552c04c83c56fe162f597dbe7ce20ab6a
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-25 20:56:32 +0330

    adds user reducer

--- client/src/reducers/reducer-user.ts
+++ client/src/reducers/reducer-user.ts
@@ -1,9 +1,16 @@
 import {Reducer} from "redux";
 import {AllActionTypes} from "../actions/base-action";
 import {secureUserInfo} from "../../../src/shared/utils";
+import {FETCH_USER_FULFILLED} from "../actions/creator-fetch-user-fulfilled";
 
-export default ((state, action) => {
-    switch (action.type) {
+const reducer: Reducer<ReturnType<typeof secureUserInfo>, AllActionTypes> =
+    (state, action) => {
+        switch (action.type) {
+            case FETCH_USER_FULFILLED:
+                return action.payload;
+            default:
+                return state;
+        }
+    };
 
-    }
-}) as Reducer<ReturnType<typeof secureUserInfo>, AllActionTypes>;
+export default reducer;
