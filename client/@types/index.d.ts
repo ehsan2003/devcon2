761a250bb15fcbf4f1ae035cbf1ac3a9da23bbae
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-30 11:24:10 +0330

    removes create-react-app client

--- client/@types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import {RequestHandler} from "express";
-
-export declare type ResponseType<T extends RequestHandler> = T extends RequestHandler<infer T1, infer T2, infer T3, infer T4> ? T2 : never ;
--- client/src/epics/index.ts
+++
@@ -1,5 +0,0 @@
-import {combineEpics} from "redux-observable";
-import categoriesFetchFulfilled from './categories-fetch-fulfilled';
-
-
-export default combineEpics(categoriesFetchFulfilled);
