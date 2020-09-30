7b9222103465085cef433df09262d3af556747dd
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-30 22:53:22 +0330

    adds error code to error response

--- src/routes/errorHandler.ts
+++ src/routes/errorHandler.ts
@@ -3,7 +3,10 @@ import {InternalServerError, SiteError} from '@shared/errors';
 import util from 'util';
 
 export default (isDev: boolean) => (err: SiteError, req: Request, res: Response, next: NextFunction) => {
-    const responseObject: { [p: string]: any } = {msg: err.message};
+    const responseObject: { [p: string]: any } = {
+        msg: err.message,
+        code: err.code
+    };
     if (isDev) {
         responseObject.err = util.inspect(err);
         if (err instanceof InternalServerError)
