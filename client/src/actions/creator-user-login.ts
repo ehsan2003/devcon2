5d0880e7e2032ba1677ac4d101845953716661a3
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-30 15:18:20 +0330

    modifies user login action creator to receive email and password

--- client/src/actions/creator-user-login.ts
+++ client/src/actions/creator-user-login.ts
@@ -4,8 +4,16 @@ import type {ActionCreator} from 'redux';
 export const USER_LOGIN = 'USER_LOGIN';
 
 export interface ActionTypeUserLogin extends BaseAction<typeof USER_LOGIN> {
+    payload: {
+        email: string;
+        password: string;
+    };
 }
 
-export const userLogin: ActionCreator<ActionTypeUserLogin> = () => ({
+export const userLogin: ActionCreator<ActionTypeUserLogin> = (email: string, password: string) => ({
     type: USER_LOGIN
+    , payload: {
+        email,
+        password
+    }
 });
