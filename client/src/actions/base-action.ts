d0c78a2a4ad504f13a1ec6cfda8e948efbafa803
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-30 14:02:03 +0330

    adds user login action type to AllActions type

--- client/src/actions/base-action.ts
+++ client/src/actions/base-action.ts
@@ -1,7 +1,10 @@
 import {ActionTypeUserLoginFulfilled} from "@actions/creator-user-login-fulfilled";
+import {ActionTypeUserLogin} from "@actions/creator-user-login";
 
 export interface BaseAction<TypeString extends string> {
     type: TypeString;
 }
 
-export type AllActions = ActionTypeUserLoginFulfilled;
+export type AllActions =
+    ActionTypeUserLoginFulfilled
+    | ActionTypeUserLogin;
