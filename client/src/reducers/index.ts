1d2295d54bf874f50893186ac6b9f0144052094e
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-09-30 13:56:24 +0330

    adds authorization reducer to root reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,3 +1,6 @@
 import {combineReducers} from "redux";
+import authorization from "@reducers/reducer-authorization";
 
-export default combineReducers({});
+export default combineReducers({
+    authorization
+});
