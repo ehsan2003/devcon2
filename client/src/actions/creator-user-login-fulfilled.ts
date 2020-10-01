8605b4f0e02e3cd7c0f244e74b01682dac8f992c
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-01 23:51:22 +0330

    modifies action creators to use new template

--- client/src/actions/creator-user-login-fulfilled.ts
+++ client/src/actions/creator-user-login-fulfilled.ts
@@ -1,13 +1,12 @@
-import {BaseAction} from "@actions/base-action";
-import {ActionCreator} from "redux";
+import {Action} from "redux";
 
 export const USER_LOGIN_FULFILLED = 'USER_LOGIN_FULFILLED';
 
-export interface ActionTypeUserLoginFulfilled extends BaseAction<typeof USER_LOGIN_FULFILLED> {
+export interface ActionTypeUserLoginFulfilled extends Action<typeof USER_LOGIN_FULFILLED> {
     payload: { token: string };
 }
 
-export const userLoginFulfilled: ActionCreator<ActionTypeUserLoginFulfilled> = (token: string) => ({
+export const userLoginFulfilled = (token: string): ActionTypeUserLoginFulfilled => ({
     type: USER_LOGIN_FULFILLED
     , payload: {token}
 });
--- client/src/actions/creator-user-login.ts
+++ client/src/actions/creator-user-login.ts
@@ -1,16 +1,15 @@
-import {BaseAction} from "@actions/base-action";
-import type {ActionCreator} from 'redux';
+import type {Action} from 'redux';
 
 export const USER_LOGIN = 'USER_LOGIN';
 
-export interface ActionTypeUserLogin extends BaseAction<typeof USER_LOGIN> {
+export interface ActionTypeUserLogin extends Action<typeof USER_LOGIN> {
     payload: {
         email: string;
         password: string;
     };
 }
 
-export const userLogin: ActionCreator<ActionTypeUserLogin> = (email: string, password: string) => ({
+export const userLogin = (email: string, password: string): ActionTypeUserLogin => ({
     type: USER_LOGIN
     , payload: {
         email,
