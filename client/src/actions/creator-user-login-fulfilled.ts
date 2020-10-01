d198d043d485cf1a99233c48ffb21ffa60e3a9c2
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-01 15:49:37 +0330

    adds type to user login fulfilled action creator

--- client/src/actions/creator-user-login-fulfilled.ts
+++ client/src/actions/creator-user-login-fulfilled.ts
@@ -1,4 +1,5 @@
 import {BaseAction} from "@actions/base-action";
+import {ActionCreator} from "redux";
 
 export const USER_LOGIN_FULFILLED = 'USER_LOGIN_FULFILLED';
 
@@ -6,7 +7,7 @@ export interface ActionTypeUserLoginFulfilled extends BaseAction<typeof USER_LOG
     payload: { token: string };
 }
 
-export const userLoginFulfilled = (token: string) => ({
+export const userLoginFulfilled: ActionCreator<ActionTypeUserLoginFulfilled> = (token: string) => ({
     type: USER_LOGIN_FULFILLED
     , payload: {token}
 });
