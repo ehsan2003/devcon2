b967075cc7e9aa9a380eafbbff3389aaefb3ae7e
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-02 09:50:49 +0330

    specifies types to create epic middleware

--- client/src/store.ts
+++ client/src/store.ts
@@ -2,9 +2,11 @@ import {applyMiddleware, compose, createStore} from "redux";
 import {createEpicMiddleware} from "redux-observable";
 import epics from '@epics/index';
 import rootReducer from './reducers';
+import {AllActions} from "@actions/index";
+import {ReducerState} from "react";
 
 const composeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const epicMiddleware = createEpicMiddleware();
+const epicMiddleware = createEpicMiddleware<AllActions, AllActions, ReducerState<typeof rootReducer>>();
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(epicMiddleware)));
 
 epicMiddleware.run(epics);
