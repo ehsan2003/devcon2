6ccf512137f1828558495e1471363a8a7b577946
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-02 11:24:20 +0330

    modifies error handler in user login epic and removes unnecessary console.logs

--- client/src/epics/epic-user-login.ts
+++ client/src/epics/epic-user-login.ts
@@ -8,40 +8,25 @@ import {UsersLoginRequestHandler} from "../../../src/routes/api/v1/users/login";
 import {AllActions} from "@actions/index";
 import rootReducer from '@reducers/index';
 import {ReducerState} from "react";
-import {ErrorCodes} from "@shared/utils";
+import {of} from "rxjs";
+import {ActionTypeUserLoginRejected, userLoginRejected} from "@actions/creator-user-login-rejected";
 
 type ExtractResponseType<T extends RequestHandler> = T extends RequestHandler<infer T1, infer T2, infer T3, infer T4> ? T2 : never;
-export const epicUserLogin: Epic<AllActions, ActionTypeUserLoginFulfilled, ReducerState<typeof rootReducer>> = (action$) =>
+export const epicUserLogin: Epic<AllActions, ActionTypeUserLoginFulfilled | ActionTypeUserLoginRejected, ReducerState<typeof rootReducer>> = (action$) =>
     action$.pipe(
         ofType<AllActions, ActionTypeUserLogin>(USER_LOGIN),
-        mergeMap(action => {
-                console.log('actioning');
-                return ajax({
-                    url: '/api/v1/users/login'
-                    , method: 'POST'
-                    , body: action.payload
-                }).pipe(
-                    map(response => {
-                        console.log(response);
-                        const responseJson = response.response as ExtractResponseType<UsersLoginRequestHandler>;
-                        return userLoginFulfilled(responseJson.token);
-                    }),
-                    catchError((err, caught) => {
-                        console.log(err);
-                        if (err.name === 'AjaxError') {
-                            switch (err.code as keyof typeof ErrorCodes) {
-                                case ErrorCodes.ERROR_USER_LOGIN_$_INVALID_EMAIL:
-
-                            }
-                        }
-                        caught.subscribe((v) => {
-                            console.log('subscriber', v);
-                        });
-                        return caught;
-                    })
-                );
-            }
-        )
-    )
-;
+        mergeMap(action =>
+            ajax({
+                url: '/api/v1/users/login'
+                , method: 'POST'
+                , body: action.payload
+            }).pipe(
+                map(response => {
+                    console.log(response);
+                    const responseJson = response.response as ExtractResponseType<UsersLoginRequestHandler>;
+                    return userLoginFulfilled(responseJson.token);
+                }),
+                catchError((err, caught) => of(userLoginRejected(err)))
+            )
+        ));
 export default epicUserLogin;
