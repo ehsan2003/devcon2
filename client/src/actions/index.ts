cd4ce8aafd77f51283932594140ee6c6e93115ae
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-02 09:46:53 +0330

    adds error types ( api error codes ) to AllActions type as actions types

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,9 +1,12 @@
 import {ActionTypeUserLoginFulfilled, userLoginFulfilled} from "@actions/creator-user-login-fulfilled";
 import {ActionTypeUserLogin, userLogin} from "@actions/creator-user-login";
+import {ErrorCodes} from "@shared/utils";
 
 export type AllActionCreators =
     typeof userLogin |
     typeof userLoginFulfilled;
 export type AllActions =
     ActionTypeUserLoginFulfilled
-    | ActionTypeUserLogin;
+    | ActionTypeUserLogin
+    | { type: keyof typeof ErrorCodes, error: any }
+    ;
