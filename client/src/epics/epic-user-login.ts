1c52a0ca170c8f628b33339a6c65d834a02a568f
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-02 11:28:58 +0330

    moves ExtractResponseType type deceleration to utils

--- client/src/epics/epic-user-login.ts
+++ client/src/epics/epic-user-login.ts
@@ -3,15 +3,14 @@ import {catchError, map, mergeMap} from "rxjs/operators";
 import {ajax} from "rxjs/ajax";
 import {ActionTypeUserLogin, USER_LOGIN} from "@actions/creator-user-login";
 import {ActionTypeUserLoginFulfilled, userLoginFulfilled} from "@actions/creator-user-login-fulfilled";
-import {RequestHandler} from "express";
 import {UsersLoginRequestHandler} from "../../../src/routes/api/v1/users/login";
 import {AllActions} from "@actions/index";
 import rootReducer from '@reducers/index';
 import {ReducerState} from "react";
 import {of} from "rxjs";
 import {ActionTypeUserLoginRejected, userLoginRejected} from "@actions/creator-user-login-rejected";
+import {ResponseType} from '@shared/utils';
 
-type ExtractResponseType<T extends RequestHandler> = T extends RequestHandler<infer T1, infer T2, infer T3, infer T4> ? T2 : never;
 export const epicUserLogin: Epic<AllActions, ActionTypeUserLoginFulfilled | ActionTypeUserLoginRejected, ReducerState<typeof rootReducer>> = (action$) =>
     action$.pipe(
         ofType<AllActions, ActionTypeUserLogin>(USER_LOGIN),
@@ -22,7 +21,7 @@ export const epicUserLogin: Epic<AllActions, ActionTypeUserLoginFulfilled | Acti
                 , body: action.payload
             }).pipe(
                 map(response =>
-                    userLoginFulfilled((response.response as ExtractResponseType<UsersLoginRequestHandler>).token)
+                    userLoginFulfilled((response.response as ResponseType<UsersLoginRequestHandler>).token)
                 ),
                 catchError((err, caught) => of(userLoginRejected(err)))
             )
---
+++ client/src/shared/utils/response-type.ts
@@ -0,0 +1,3 @@
+import {RequestHandler} from "express";
+
+export type ResponseType<T extends RequestHandler> = T extends RequestHandler<infer T1, infer T2, infer T3, infer T4> ? T2 : never;
