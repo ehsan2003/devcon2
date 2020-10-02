67659a58d865a321525fafe2503acdc3f2237f19
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-02 09:52:27 +0330

    modifies state type and effective actions on authorization reducer

--- client/src/reducers/reducer-authorization.ts
+++ client/src/reducers/reducer-authorization.ts
@@ -1,14 +1,29 @@
 import {Reducer} from "redux";
-import {AllActions} from "@actions/base-action";
+import {AllActions} from "@actions/index";
 import {USER_LOGIN_FULFILLED} from "@actions/creator-user-login-fulfilled";
 
-export type StateAuthorization = string | undefined;
+export type StateAuthorization = {
+    error: null | Error, data: null | string
+};
 
 const authorization: Reducer<StateAuthorization, AllActions> =
-    (state, action) => {
+    (state = {
+        error: null
+        , data: null
+    }, action) => {
         switch (action.type) {
             case USER_LOGIN_FULFILLED:
-                return action.payload.token;
+                return {data: action.payload.token, error: null};
+            case "ERROR_USER_LOGIN_$_INVALID_EMAIL":
+                return {
+                    data: null,
+                    error: action.error
+                };
+            case "ERROR_USER_LOGIN_$_INVALID_PASSWORD":
+                return {
+                    data: null,
+                    error: action.error
+                };
             default:
                 return state;
         }
