9136f55243623f2fa5cdbc5c8244fb518086b976
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-05 19:17:56 +0330

    makes Header component a functional one

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,53 +1,145 @@
-import React, {FunctionComponent, ReducerState, useState} from 'react';
-import {connect} from 'react-redux';
-import rootReducer from '@reducers/index';
+import React, {useState} from 'react';
+import {connect, MapDispatchToProps, MapStateToProps} from 'react-redux';
 import {makeStyles} from "@material-ui/styles";
-import {AppBar, Drawer, Hidden, IconButton, Theme, Toolbar} from "@material-ui/core";
-import {Menu as MenuIcon} from "@material-ui/icons";
+import {
+    AppBar,
+    Button,
+    Drawer,
+    Hidden,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Theme,
+    Toolbar,
+    Typography,
+    Zoom
+} from "@material-ui/core";
+import {
+    AccountBox as ProfileIcon,
+    Close as CloseIcon,
+    Dashboard as DashboardIcon,
+    Home as HomeIcon,
+    ImportContacts as ImportContactsIcon,
+    Menu as MenuIcon
+} from "@material-ui/icons";
 import containerTheme from "../theme";
+import {RootState} from "../reducers";
+import {dispatchType} from "@shared/utils";
 
+export interface OwnProps {}
 
-function mapStateToProps(state: ReducerState<typeof rootReducer>) {
-    return {};
-}
+const mapDispatchToProps: MapDispatchToProps<{}, OwnProps> = {};
+const mapStateToProps: MapStateToProps<{ authorization: RootState['authorization'] }, OwnProps, RootState> = state => ({authorization: state.authorization});
 
-const mapDispatchToProps = {};
+export interface Props extends ReturnType<typeof mapStateToProps>, OwnProps, dispatchType<typeof mapDispatchToProps> {}
 
-// propTypes
-interface Props extends ReturnType<typeof mapStateToProps> {
-
-}
-
+const drawerWidth = 240;
 const useStyles = makeStyles((theme: Theme) => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 101
+    },
     root: {
         color: 'red',
     },
     menuIcon: {
-        marginRight: theme.spacing(2)
+        marginRight: theme.spacing(2),
+        position: 'relative'
+    },
+    menuIconIcon: {
+        position: 'absolute',
+        top: 0,
+        left: 0
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    brandName: {
+        flexGrow: 1,
+        userSelect: 'none'
+    }, menuButton: {
+        color: 'inherit',
+        marginRight: theme.spacing(1),
+        fontWeight: 'bold'
     }
 }));
-const Header: FunctionComponent<Props> = (props) => {
+
+const Header: React.FC<Props> = (props => {
+    const menuItems = [
+        {
+            link: '/home',
+            text: 'home',
+            icon: HomeIcon
+        }, {
+            link: '/blog',
+            text: 'blog',
+            icon: ImportContactsIcon
+        },
+        ...props.authorization.data ? [
+            {
+                link: '/dashboard',
+                text: 'dashboard',
+                icon: DashboardIcon,
+            }, {
+                link: '/profile',
+                text: 'profile',
+                icon: ProfileIcon
+            }
+        ] : []
+    ];
     const classes = useStyles(containerTheme);
+    props.authorization.data = 'hello';
     const [mainMenuOpen, setMainMenuOpen] = useState(false);
     return (
         <React.Fragment>
-            <AppBar>
+            <AppBar className={classes.appBar} position={'fixed'}>
                 <Toolbar>
                     <Hidden smUp>
-                        <IconButton color={'inherit'} className={classes.menuIcon}>
-                            <MenuIcon onClick={() => setMainMenuOpen(true)}/>
+                        <IconButton onClick={() => setMainMenuOpen(!mainMenuOpen)} color={'inherit'}
+                                    className={classes.menuIcon}>
+                            <Zoom in={mainMenuOpen} unmountOnExit><CloseIcon className={classes.menuIconIcon}/></Zoom>
+                            <Zoom in={!mainMenuOpen} unmountOnExit><MenuIcon className={classes.menuIconIcon}/></Zoom>
                         </IconButton>
                     </Hidden>
+                    <Typography variant={'h6'} className={classes.brandName}>
+                        Devcon
+                    </Typography>
+                    <Hidden xsDown
+                    >{menuItems.map(({text}) =>
+                        <Button className={classes.menuButton}>
+                            {text}
+                        </Button>
+                    )}</Hidden>
                 </Toolbar>
             </AppBar>
-            <Drawer open={mainMenuOpen}>
+            <Drawer
+                open={mainMenuOpen}
+                onClose={() => setMainMenuOpen(false)}
+                className={classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper
+                }}
+            >
+                <Toolbar/>
+                <List>
+                    {menuItems.map(({icon: Icon, text, secondary}) =>
+                        <ListItem button>
+                            <Link>
+                                <ListItemIcon>
+                                    <Icon/>
+                                </ListItemIcon>
+                                <ListItemText primary={text} secondary={secondary}/></Link>
+                        </ListItem>
+                    )}
 
+                </List>
             </Drawer>
         </React.Fragment>
     );
-};
+});
 
-export default connect(
-    mapStateToProps
-    , mapDispatchToProps
-)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
