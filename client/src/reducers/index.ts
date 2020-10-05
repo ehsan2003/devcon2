da92f6338002d1526ce2ba2a2dfb858db2dea460
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-05 15:20:17 +0330

    adds RootState export expression to root reducer ( which is type of the state )

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,6 +1,9 @@
 import {combineReducers} from "redux";
 import authorization from "@reducers/reducer-authorization";
+import {ReducerState} from "react";
 
-export default combineReducers({
+const rootReducer = combineReducers({
     authorization
 });
+export default rootReducer;
+export type RootState = ReducerState<typeof rootReducer>;
