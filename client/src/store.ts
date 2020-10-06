5c798cf9f3273715ac5950969e01955c58f46a99
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-06 17:38:39 +0330

    adds window resize action dispatcher to store

--- client/src/store.ts
+++ client/src/store.ts
@@ -4,10 +4,16 @@ import epics from '@epics/index';
 import rootReducer from './reducers';
 import {AllActions} from "@actions/index";
 import {ReducerState} from "react";
+import {deviceWidthChanged} from "@actions/creator-device-width-changed";
 
 const composeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const epicMiddleware = createEpicMiddleware<AllActions, AllActions, ReducerState<typeof rootReducer>>();
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(epicMiddleware)));
+
+window.addEventListener('resize', () =>
+    store.dispatch(deviceWidthChanged(window.innerWidth, window.innerHeight))
+);
+
 
 epicMiddleware.run(epics);
 
