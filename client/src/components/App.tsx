a349e22e23b5ba0ddea5973e0e6834774e70c068
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-07 01:47:20 +0330

    adds all added components ( on the previous commit to App.tsx with router )

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import Header from "@components/Header";
+import {Route, Switch} from "react-router";
+import Home from "@components/Home";
+import About from "@components/About";
+import Contact from "@components/Contact";
+import Blog from "@components/Blog";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Switch>
+                    <Route path={'/'} exact><Home/></Route>
+                    <Route path={'/blog'}><Blog/></Route>
+                    <Route path={'/about'}><About/></Route>
+                    <Route path={'/contact'}><Contact/></Route>
+                </Switch>
             </div>
         );
     }
