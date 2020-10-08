e343f766de3ef1d6ae4ce79110bc9307e9fc7c82
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-08 18:47:54 +0330

    adds current user reducer

---
+++ client/src/reducers/reducer-current-user.ts
@@ -0,0 +1,32 @@
+import {Reducer} from "redux";
+import {AllActions} from "@actions/index";
+import produce from 'immer';
+import {Roles} from "../../../src/shared/utils";
+import {USER_INFO_FULFILLED} from "@actions/creator-user-info-fulfilled";
+
+export interface StateCurrentUser {
+    error: null | any;
+    data: null | {
+        username: string;
+        _id: string;
+        role: Roles;
+    };
+}
+
+const initialState: StateCurrentUser = {
+    error: null,
+    data: null
+};
+
+const currentUser: Reducer<StateCurrentUser, AllActions> = (
+    mutableState = initialState
+    , action) => produce(mutableState, state => {
+    switch (action.type) {
+        case USER_INFO_FULFILLED:
+            return action.payload;
+        default:
+            return state;
+    }
+});
+
+export default currentUser;
