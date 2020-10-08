bafba5b5a7efe9fd76c675b8b57511f51d68963e
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-08 15:01:22 +0330

    modifies return type of secureUserInfo function

--- src/shared/utils/secureUserInfo.ts
+++ src/shared/utils/secureUserInfo.ts
@@ -1,4 +1,4 @@
-import {IUserDoc} from "@models/User";
+import {IUser, IUserDoc} from "@models/User";
 import {extractProps} from "@shared/utils/extractProps";
 
-export const secureUserInfo = (user: IUserDoc) => extractProps(user.toObject(), 'email', 'username', '_id', 'role');
+export const secureUserInfo = (user: IUserDoc) => extractProps(user.toObject() as IUser & { _id: string }, 'email', 'username', '_id', 'role');
