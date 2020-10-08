5e312ee1f143683a9dafb57b03a778bc41d8e1e3
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-08 15:29:55 +0330

    adds user info action result to user login epic
    adds some code to send "user info" action alongside "user login fulfilled"
    action as result

--- client/src/epics/epic-user-login.ts
+++ client/src/epics/epic-user-login.ts
@@ -1,5 +1,5 @@
 import {Epic, ofType} from "redux-observable";
-import {catchError, map, mergeMap} from "rxjs/operators";
+import {catchError, mergeMap} from "rxjs/operators";
 import {ajax} from "rxjs/ajax";
 import {ActionTypeUserLogin, USER_LOGIN} from "@actions/creator-user-login";
 import {ActionTypeUserLoginFulfilled, userLoginFulfilled} from "@actions/creator-user-login-fulfilled";
@@ -10,17 +10,19 @@ import {ReducerState} from "react";
 import {of} from "rxjs";
 import {ActionTypeUserLoginRejected, userLoginRejected} from "@actions/creator-user-login-rejected";
 import {ResponseType} from '@shared/utils';
+import {ActionTypeUserInfo, userInfo} from "@actions/creator-user-info";
 
-export const epicUserLogin: Epic<AllActions, ActionTypeUserLoginFulfilled | ActionTypeUserLoginRejected, ReducerState<typeof rootReducer>> = (action$) =>
+export const epicUserLogin: Epic<AllActions, (ActionTypeUserLoginFulfilled | ActionTypeUserInfo) | ActionTypeUserLoginRejected, ReducerState<typeof rootReducer>> = (action$) =>
     action$.pipe(
         ofType<AllActions, ActionTypeUserLogin>(USER_LOGIN),
         mergeMap(action =>
+
             ajax({
                 url: '/api/v1/users/login'
                 , method: 'POST'
                 , body: action.payload
             }).pipe(
-                map(response => userLoginFulfilled((response.response as ResponseType<UsersLoginRequestHandler>).token)),
+                mergeMap(response => of(userLoginFulfilled((response.response as ResponseType<UsersLoginRequestHandler>).token), userInfo())),
                 catchError((err, caught) => of(userLoginRejected(err)))
             )
         ));
