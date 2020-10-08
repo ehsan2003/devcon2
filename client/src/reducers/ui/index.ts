bd2e5f159dac0dabc6460cf73d349ea759bfbba1
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-08 19:32:00 +0330

    adds main progress ui reducer

--- client/src/reducers/ui/index.ts
+++ client/src/reducers/ui/index.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import mainMenuOpen from "./reducer-main-menu-open";
 import deviceWidth from './reducer-device-width';
+import mainProgress from "@reducers/ui/reducer-main-progress";
 
 const Ui = combineReducers({
     mainMenuOpen,
-    deviceWidth
+    deviceWidth,
+    mainProgress
 });
 
 export default Ui;
