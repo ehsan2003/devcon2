fd533d12be1d612c80e9d00c2a1451661911acef
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-08 15:08:20 +0330

    adds ActionTypeUserInfo to AllActions

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -3,10 +3,12 @@ import {ActionTypeUserLogin} from "@actions/creator-user-login";
 import {ActionTypeUserLoginRejected} from "@actions/creator-user-login-rejected";
 import {ActionTypeMainMenuOpenSet} from "@actions/creator-main-menu-open-set";
 import {ActionTypeDeviceWidthChanged} from "@actions/creator-device-width-changed";
+import {ActionTypeUserInfo} from "@actions/creator-user-info";
 
 export type AllActions =
     ActionTypeUserLoginFulfilled
     | ActionTypeUserLogin
     | ActionTypeUserLoginRejected
     | ActionTypeMainMenuOpenSet
-    | ActionTypeDeviceWidthChanged;
+    | ActionTypeDeviceWidthChanged
+    | ActionTypeUserInfo;
