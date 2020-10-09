8405251b5bc66579e6a44c61615aa78be4961807
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-09 15:38:06 +0330

    adds login dialog reducer

---
+++ client/src/reducers/ui/reducer-login-dialog.ts
@@ -0,0 +1,31 @@
+import {Reducer} from "redux";
+import {AllActions} from "@actions/index";
+import produce from 'immer';
+
+export interface StateLoginDialog {
+    error: null | Error;
+    data: {
+        open: boolean;
+    };
+}
+
+const initialState: StateLoginDialog = {
+    error: null,
+    data: {
+        open: false
+    }
+};
+
+const loginDialog: Reducer<StateLoginDialog, AllActions> = (
+    mutableState = initialState
+    , action) => produce(mutableState, state => {
+    switch (action.type) {
+        case "LOGIN_DIALOG_OPEN_SET":
+            state.data.open = action.payload;
+            break;
+        default:
+            return state;
+    }
+});
+
+export default loginDialog;
