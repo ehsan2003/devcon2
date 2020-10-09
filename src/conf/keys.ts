890b5af6d0b42984701f34caadb17a53a3825bfc
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-09 23:25:45 +0330

    adds recaptcha keys to @conf/keys

--- src/conf/keys.ts
+++ src/conf/keys.ts
@@ -1,5 +1,9 @@
 export default {
     mongoUri: 'mongodb://localhost/devcon-dev2',
     secretOrKey: 'secret',
-    smtpUrl: 'smtp://127.0.0.1:1025'
+    smtpUrl: 'smtp://127.0.0.1:1025',
+    recaptcha:{
+        siteKey:'6LcgodUZAAAAAKduF5gdRdSM3MZznkfIKE9Anya0',
+        secretKey:'6LcgodUZAAAAAEJsLMJ7D1Xx0HILf_SUpItnfBvJ'
+    }
 };
