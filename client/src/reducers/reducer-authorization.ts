976d8b3e0b27ed39a62404244de3284ded441436
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-10 11:42:06 +0330

    adds default value of localStorage authorization item to authorization reducer

--- client/src/reducers/reducer-authorization.ts
+++ client/src/reducers/reducer-authorization.ts
@@ -6,11 +6,12 @@ import {USER_LOGIN_REJECTED} from "@actions/creator-user-login-rejected";
 export type StateAuthorization = {
     error: null | Error, data: null | string
 };
+const defaultState: StateAuthorization = {
+    error: null,
+    data: localStorage.getItem('authorization')
+};
 const authorization: Reducer<StateAuthorization, AllActions> =
-    (state = {
-        error: null
-        , data: null
-    }, action) => {
+    (state = defaultState, action) => {
         switch (action.type) {
             case USER_LOGIN_FULFILLED:
                 return {data: action.payload.token, error: null};
