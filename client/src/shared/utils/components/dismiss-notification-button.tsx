df7604bb66ab802b57b67521d7b4c8e12cbba893
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-19 14:37:59 +0330

    modifies dismiss-notification-button.tsx
    changes key prop to notificationKey because the key is react builtin

--- client/src/shared/utils/components/dismiss-notification-button.tsx
+++ client/src/shared/utils/components/dismiss-notification-button.tsx
@@ -1,11 +1,15 @@
 import {SnackbarKey} from "notistack";
 import {useDispatch} from "react-redux";
-import {Button} from "@material-ui/core";
+import {Button, ButtonProps} from "@material-ui/core";
 import {dismissNotification} from "@actions/ui";
 import React from "react";
 
-const DismissButton = (key: SnackbarKey) => {
+interface Props extends ButtonProps {
+    notificationKey: SnackbarKey;
+}
+
+const DismissButton: React.FC<Props> = ({notificationKey, ...buttonProps}) => {
     const dispatch = useDispatch();
-    return <Button onClick={() => dispatch(dismissNotification(key))}>dismiss</Button>;
+    return <Button onClick={() => dispatch(dismissNotification(notificationKey))} {...buttonProps}>dismiss</Button>;
 };
 export default DismissButton;
