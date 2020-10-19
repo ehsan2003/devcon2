084d6f6339314d7709f5a4d4861bf69311f7c99b
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-19 11:54:49 +0330

    adds notification-related action types to notification reducer

--- client/src/reducers/ui/reducer-notifications.ts
+++ client/src/reducers/ui/reducer-notifications.ts
@@ -2,14 +2,15 @@ import {Reducer} from "redux";
 import {AllActions} from "@actions/index";
 import produce from 'immer';
 import {OptionsObject, SnackbarKey} from "notistack";
+import {ADD_NOTIFICATION, DISMISS_NOTIFICATION, REMOVE_NOTIFICATION} from "@actions/ui";
 
 export interface StateNotifications {
-    error: null ;
+    error: null;
     data: {
-        key:SnackbarKey;
-        message:string;
-        options?:OptionsObject;
-        dismissed:boolean;
+        key: SnackbarKey;
+        message: string;
+        options?: OptionsObject;
+        dismissed: boolean;
     }[];
 }
 
@@ -22,7 +23,19 @@ const notifications: Reducer<StateNotifications, AllActions> = (
     mutableState = initialState
     , action) => produce(mutableState, state => {
     switch (action.type) {
-        
+        case ADD_NOTIFICATION:
+            (state.data as StateNotifications['data']).push(action.payload);
+            break;
+        case REMOVE_NOTIFICATION:
+            state.data = state.data.filter(({key}) => key !== action.payload.key);
+            break;
+        case DISMISS_NOTIFICATION:
+            state.data = state.data.map((notification) =>
+                (action.payload.dismissAll || action.payload.key === notification.key) ? {
+                    ...notification,
+                    dismissed: true
+                } : notification);
+            break;
         default:
             return state;
     }
