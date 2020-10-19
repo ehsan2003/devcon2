962020668a44fb5af2f61d617b5f8a1f5067bc27
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-19 14:53:35 +0330

    moves input values ( including captcha ) to login dialog reducer

--- client/src/reducers/ui/reducer-login-dialog.ts
+++ client/src/reducers/ui/reducer-login-dialog.ts
@@ -3,18 +3,25 @@ import {AllActions} from "@actions/index";
 import produce from 'immer';
 import {USER_LOGIN_FULFILLED} from "@actions/ajax/creator-user-login-fulfilled";
 import {LOGIN_DIALOG_OPEN_SET} from "@actions/ui/creator-login-dialog-open-set";
+import {LOGIN_DIALOG_CAPTCHA_SET, LOGIN_DIALOG_SET_EMAIL, LOGIN_DIALOG_SET_PASSWORD} from "@actions/ui";
 
 export interface StateLoginDialog {
     error: null | Error;
     data: {
         open: boolean;
+        captchaValue: string | null;
+        password: string;
+        email: string;
     };
 }
 
 const initialState: StateLoginDialog = {
     error: null,
     data: {
-        open: false
+        password: '',
+        email: '',
+        captchaValue: null,
+        open: false,
     }
 };
 
@@ -22,11 +29,20 @@ const loginDialog: Reducer<StateLoginDialog, AllActions> = (
     mutableState = initialState
     , action) => produce(mutableState, state => {
     switch (action.type) {
+        case LOGIN_DIALOG_CAPTCHA_SET:
+            state.data.captchaValue = action.payload;
+            break;
+        case LOGIN_DIALOG_SET_EMAIL:
+            state.data.email = action.payload;
+            break;
+        case LOGIN_DIALOG_SET_PASSWORD:
+            state.data.password = action.payload;
+            break;
         case LOGIN_DIALOG_OPEN_SET:
             state.data.open = action.payload;
             break;
         case USER_LOGIN_FULFILLED:
-            state.data.open=false;
+            state.data.open = false;
             break;
         default:
             return state;
