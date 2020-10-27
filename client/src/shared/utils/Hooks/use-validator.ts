3113c368e4034426a59150d69e815b9e4113e55d
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-27 14:43:01 +0330

    removes async compatibility of useValidator hook

--- client/src/shared/utils/Hooks/use-validator.ts
+++ client/src/shared/utils/Hooks/use-validator.ts
@@ -1,5 +1,5 @@
 import {JSONSchema7} from 'json-schema';
-import {useEffect, useState} from 'react';
+import {useMemo, useState} from 'react';
 import Ajv, {ErrorObject} from 'ajv';
 
 const ajv = new Ajv({
@@ -14,32 +14,10 @@ export interface ValidationResult {
 
 export default (schema: JSONSchema7, data: any) => {
     const [isValid, setIsValid] = useState(false);
-    const [isPending, setIsPending] = useState(true);
-    const [errors, setErrors] = useState<ErrorObject[] | null | undefined>(null);
-    useEffect(() => {
-        const tmp = ajv.validate(schema, data);
-        if (isPromise(tmp)) {
-            if (!isPending)
-                setIsPending(true);
-            (tmp as PromiseLike<any>).then(result => {
-                setIsPending(false);
-                setIsValid(result);
-                setErrors(ajv.errors);
-            });
-        } else {
-            if (isPending)
-                setIsPending(false);
-            setIsValid(tmp as boolean);
-            setErrors(ajv.errors);
-        }
-    }, [data, isPending]);
-    return {
-        isValid,
-        isPending,
-        errors
-    };
+    const [errors, setErrors] = useState<null | undefined | ErrorObject[]>(null);
+    return useMemo(() => {
+        setIsValid(ajv.validate(schema, data) as boolean);
+        setErrors(ajv.errors);
+        return {isValid, errors};
+    }, [data]);
 };
-
-function isPromise(data: any) {
-    return !!data.then;
-}
