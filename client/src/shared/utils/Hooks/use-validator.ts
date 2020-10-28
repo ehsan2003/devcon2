81c3482016f1280f574fea7cd7ffa09e2371445b
ehsan ilbeigy <ehsan2003.2003.382@gmail.com>
2020-10-28 23:32:22 +0330

    completely modifies useValidator hook
    now it's not using react states
    adds ajv-errors ajv plugin to validator
    modifies Ajv options ( adds jsonPointers )
    removes isPending which was for async validation

--- client/src/shared/utils/Hooks/use-validator.ts
+++ client/src/shared/utils/Hooks/use-validator.ts
@@ -1,23 +1,18 @@
 import {JSONSchema7} from 'json-schema';
-import {useMemo, useState} from 'react';
+import {useMemo} from 'react';
 import Ajv, {ErrorObject} from 'ajv';
+import applyErrors from 'ajv-errors';
 
 const ajv = new Ajv({
-    allErrors:true
+    allErrors: true, jsonPointers: true
 });
-
+applyErrors(ajv);
 export interface ValidationResult {
-    isPending: boolean;
     isValid: boolean;
     errors: ErrorObject[] | null | undefined;
 }
 
-export default (schema: JSONSchema7, data: any) => {
-    const [isValid, setIsValid] = useState(false);
-    const [errors, setErrors] = useState<null | undefined | ErrorObject[]>(null);
-    return useMemo(() => {
-        setIsValid(ajv.validate(schema, data) as boolean);
-        setErrors(ajv.errors);
-        return {isValid, errors};
-    }, [data]);
-};
+export default (schema: JSONSchema7, data: any) => useMemo(() => ({
+    isValid:ajv.validate(schema,data),
+    errors:ajv.errors
+}), [data]);
